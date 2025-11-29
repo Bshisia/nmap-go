0691c0ed7702ee8e6d261cf2011a5189b05488d4
bshisia <shisiabrian7@gmail.com>
2025-11-29 17:46:41 +0300

    feat: enhance command-line usage to support SYN scan option

--- main.go
+++ main.go
@@ -9,25 +9,34 @@ import (
 
 func main() {
 	if len(os.Args) < 3 {
-		fmt.Println("Usage: go run main.go [-sV] <host> <port-range>")
-		fmt.Println("Example: go run main.go -sV 192.168.1.1 80-443")
+		fmt.Println("Usage: go run main.go [-sV|-sS] <host> <port-range>")
+		fmt.Println("Example: go run main.go -sS 192.168.1.1 80-443")
 		os.Exit(1)
 	}
 
-	var serviceDetection bool
+	var serviceDetection, synScan bool
 	var host, portRange string
 
-	if os.Args[1] == "-sV" {
+	switch os.Args[1] {
+	case "-sV":
 		serviceDetection = true
 		host = os.Args[2]
 		portRange = os.Args[3]
-	} else {
+	case "-sS":
+		synScan = true
+		host = os.Args[2]
+		portRange = os.Args[3]
+	default:
 		host = os.Args[1]
 		portRange = os.Args[2]
 	}
 
 	ports := utils.ParsePortRange(portRange)
-	scanner.ScanPorts(host, ports, serviceDetection)
+	if synScan {
+		scanner.SynScan(host, ports)
+	} else {
+		scanner.ScanPorts(host, ports, serviceDetection)
+	}
 }
 
 
