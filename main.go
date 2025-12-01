2f7c0c07a59beedbc07b67a40b28743cef84b169
bshisia <shisiabrian7@gmail.com>
2025-12-01 11:20:01 +0300

    feat: enhance command-line arguments to support aggressive scan option

--- main.go
+++ main.go
@@ -8,31 +8,34 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: go run main.go [-sV|-sS] <host> <port-range>")
-		fmt.Println("Example: go run main.go -sS 192.168.1.1 80-443")
+	var serviceDetection, synScan, aggressive bool
+	var host, portRange string
+
+	switch {
+	case len(os.Args) == 4 && (os.Args[1] == "-sV" || os.Args[1] == "-sS" || os.Args[1] == "-A"):
+		switch os.Args[1] {
+		case "-sV":
+			serviceDetection = true
+		case "-sS":
+			synScan = true
+		case "-A":
+			aggressive = true
+		}
+		host = os.Args[2]
+		portRange = os.Args[3]
+	case len(os.Args) == 3:
+		host = os.Args[1]
+		portRange = os.Args[2]
+	default:
+		fmt.Println("Usage: go run main.go [-sV|-sS|-A] <host> <port-range>")
+		fmt.Println("Example: go run main.go -A 192.168.1.1 80-443")
 		os.Exit(1)
 	}
 
-	var serviceDetection, synScan bool
-	var host, portRange string
-
-	switch os.Args[1] {
-	case "-sV":
-		serviceDetection = true
-		host = os.Args[2]
-		portRange = os.Args[3]
-	case "-sS":
-		synScan = true
-		host = os.Args[2]
-		portRange = os.Args[3]
-	default:
-		host = os.Args[1]
-		portRange = os.Args[2]
-	}
-
 	ports := utils.ParsePortRange(portRange)
-	if synScan {
+	if aggressive {
+		scanner.AggressiveScan(host, ports)
+	} else if synScan {
 		scanner.SynScan(host, ports)
 	} else {
 		scanner.ScanPorts(host, ports, serviceDetection)
