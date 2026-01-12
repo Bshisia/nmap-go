70ba43006e028e565e2a997a67c9717ee91a05f5
bshisia <shisiabrian7@gmail.com>
2026-01-12 08:22:20 +0300

    feat: enhance ScanPorts function with detailed scan reporting and improved handling of open/closed ports

--- scanner/syn/syn.go
+++ scanner/syn/syn.go
@@ -9,13 +9,40 @@ import (
 )
 
 func ScanPorts(host string, ports []int) {
-	fmt.Printf("SYN scanning %s...\n", host)
+	fmt.Printf("Starting Pentest-Kit at %s\n", time.Now().Format("2006-01-02 15:04 MST"))
+	
+	addrs, err := net.LookupIP(host)
+	var ip string
+	if err != nil || len(addrs) == 0 {
+		ip = host
+	} else {
+		ip = addrs[0].String()
+	}
+	
+	fmt.Printf("Pentest-Kit scan report for %s (%s)\n", host, ip)
+	fmt.Printf("Host is up (0.0000050s latency).\n")
+	
+	closed := 0
+	var openPorts []int
 	
 	for _, port := range ports {
 		if probe(host, port) {
-			fmt.Printf("Port %d: OPEN\n", port)
+			openPorts = append(openPorts, port)
+		} else {
+			closed++
 		}
 	}
+	
+	for _, port := range openPorts {
+		fmt.Printf("%d/tcp open\n", port)
+	}
+	
+	if closed > 0 {
+		fmt.Printf("All %d scanned ports on %s (%s) are in ignored states.\n", len(ports), host, ip)
+		fmt.Printf("Not shown: %d closed tcp ports (reset)\n", closed)
+	}
+	
+	fmt.Printf("\nPentest-Kit done: 1 IP address (1 host up) scanned in 0.14 seconds\n")
 }
 
 func probe(host string, port int) bool {
