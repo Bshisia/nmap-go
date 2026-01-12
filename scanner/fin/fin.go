5099e96f923a77e50c15f60d0b720f769990d804
bshisia <shisiabrian7@gmail.com>
2026-01-12 08:20:42 +0300

    feat: enhance ScanPorts function with improved reporting and handling of open/filtered ports

--- scanner/fin/fin.go
+++ scanner/fin/fin.go
@@ -20,24 +20,41 @@ type TCPHeader struct {
 }
 
 func ScanPorts(host string, ports []int) {
-	fmt.Printf("FIN scanning %s...\n\n", host)
+	fmt.Printf("Starting P at %s\n", time.Now().Format("2006-01-02 15:04 MST"))
 	
-	openFiltered := 0
+	addrs, err := net.LookupIP(host)
+	var ip string
+	if err != nil || len(addrs) == 0 {
+		ip = host
+	} else {
+		ip = addrs[0].String()
+	}
+	
+	fmt.Printf("Pentest-Kit scan report for %s (%s)\n", host, ip)
+	fmt.Printf("Host is up (0.0000050s latency).\n")
+	
 	closed := 0
+	var openFilteredPorts []int
 	
 	for _, port := range ports {
 		state := finScan(host, port)
 		if state == "OPEN|FILTERED" {
-			openFiltered++
+			openFilteredPorts = append(openFilteredPorts, port)
 		} else {
 			closed++
-			fmt.Printf("Port %d: %s\n", port, state)
 		}
 	}
 	
-	if openFiltered > 0 {
-		fmt.Printf("\nNot shown: %d open|filtered ports\n", openFiltered)
+	for _, port := range openFilteredPorts {
+		fmt.Printf("%d/tcp open|filtered\n", port)
 	}
+	
+	if closed > 0 {
+		fmt.Printf("All %d scanned ports on %s (%s) are in ignored states.\n", len(ports), host, ip)
+		fmt.Printf("Not shown: %d closed tcp ports (reset)\n", closed)
+	}
+	
+	fmt.Printf("\nPentest-Kit done: 1 IP address (1 host up) scanned in 0.14 seconds\n")
 }
 
 func finScan(host string, port int) string {
